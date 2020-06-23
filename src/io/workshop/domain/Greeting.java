6694dcbf7438d28225320f1e434cdcc6ab2cb69f
JAK <khans.jak@gmail.com>
2020-06-23 20:52:01 +0530

    23 Jun 2020

---
+++ src/io/workshop/domain/Greeting.java
@@ -0,0 +1,9 @@
+package io.workshop.domain;
+
+public class Greeting {
+
+    public void greet(){
+        System.out.println("Hello World ! from :Java 8");
+    }
+
+}
---
+++ src/io/workshop/test/TestGreeting.java
@@ -0,0 +1,10 @@
+package io.workshop.test;
+
+import io.workshop.domain.Greeting;
+
+public class TestGreeting {
+    public static void main(String[] args) {
+        Greeting greeting = new Greeting();
+        greeting.greet();
+    }
+}
